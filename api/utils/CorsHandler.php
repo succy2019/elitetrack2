d782a21c76474ac512ff6b95262a7248abca6612
succy2019 <successoghenede2@gmail.com>
2025-10-16 04:58:02 -0500

    track

--- api/utils/CorsHandler.php
+++ api/utils/CorsHandler.php
@@ -35,7 +35,7 @@ class CorsHandler {
         }
 
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Cache-Control");
         header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Max-Age: 86400"); // Cache preflight for 24 hours
     }
